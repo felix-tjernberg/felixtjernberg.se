506a4f8686b032ca3b2a81235602eaf7efa9e09a
Felix Tjernberg <felix.tjernberg@hotmail.se>
2023-08-07 05:27:22 +0200

    Update error page styling so text is always visible and iframe is 50vh

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -4,9 +4,8 @@
 
 <div>
     {#if $page.status === 404}
-        <h1>This page is not what you are looking for</h1>
+        <h1>This is not the page you are looking for</h1>
         <iframe
-            style="width: 100%; max-width: 1280px; aspect-ratio: 16/9;"
             src="https://www.youtube-nocookie.com/embed/GO_xfR64qSk"
             title="YouTube video player, showing a scene from Star Wars Episode IV: A New Hope, where Obi-Wan Kenobi uses a Jedi mind trick to make a stormtrooper believe that these are not the droids he is looking for"
             frameborder="0"
@@ -16,7 +15,6 @@
     {:else}
         <h1>YOU DIED</h1>
         <iframe
-            style="width: 100%; max-width: 1280px; aspect-ratio: 16/9;"
             src="https://www.youtube-nocookie.com/embed/vxKBHX9Datw"
             title="YouTube video player, showing a scene from Fist of the North Star, where Upon unleashing his signature move Hokuto Hyakuretsu-ken, and tells his victim that they are already dead before his body erupts from the inside."
             frameborder="0"
@@ -27,12 +25,21 @@
 </div>
 
 <style>
+    iframe {
+        aspect-ratio: 16/9;
+        height: 50vh;
+    }
     div {
         display: grid;
         place-content: center;
-        min-height: 100vh;
     }
     div > * {
         margin: auto;
+    }
+    h1,
+    a {
+        width: 100%;
+        max-width: 100%;
+        color: var(--white);
     }
 </style>
