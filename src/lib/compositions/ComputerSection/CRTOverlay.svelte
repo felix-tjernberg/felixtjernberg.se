db8644239e353f5854a86b3e0c494c2f6118baf9
Felix Tjernberg <felix.tjernberg@hotmail.se>
2023-08-12 14:12:21 +0200

    Add pointer-events: none

--- src/lib/compositions/ComputerSection/CRTOverlay.svelte
+++ src/lib/compositions/ComputerSection/CRTOverlay.svelte
@@ -1,6 +1,6 @@
-<div class="absolute crt-effect background inset" />
+<div class="crt-effect background absolute inset" />
 <slot />
-<div class="absolute overlay inset" />
+<div class="crt-effect overlay absolute inset" />
 
 <style>
     .crt-effect.background {
@@ -10,23 +10,28 @@
     :global([data-dark-mode="false"] .crt-effect.background) {
         background: radial-gradient(transparent, #888888), #ffffff;
     }
-    .overlay::before {
+    .crt-effect.background,
+    .crt-effect.overlay,
+    .crt-effect.overlay::after,
+    .crt-effect.overlay::before {
+        pointer-events: none;
+    }
+    .crt-effect.overlay::before {
         background: linear-gradient(rgba(18, 16, 16, 0) 50%, rgba(0, 0, 0, 0.25) 50%),
             linear-gradient(90deg, rgba(255, 0, 0, 0.06), rgba(0, 255, 0, 0.02), rgba(0, 0, 255, 0.06));
         background-size: 100% 6.66px, 6.66px 100%;
         width: 100%;
         height: 100%;
     }
-    .overlay::before,
-    .overlay::after {
+    .crt-effect.overlay::before,
+    .crt-effect.overlay::after {
         pointer-events: none;
         position: absolute;
         display: block;
         content: "";
-        pointer-events: none;
         width: 100%;
     }
-    .overlay::after {
+    .crt-effect.overlay::after {
         animation: scan-line 5s linear infinite;
         background: linear-gradient(0deg, rgba(0, 0, 0, 0) 0%, rgba(255, 255, 255, 0.2) 10%, rgba(0, 0, 0, 0.1) 100%);
         height: 50px;
