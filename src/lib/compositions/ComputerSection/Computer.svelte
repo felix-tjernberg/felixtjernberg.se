05ea51dc341c292d7fc68fec0fcf8c03b1b51e87
Felix Tjernberg <felix.tjernberg@hotmail.se>
2023-08-14 16:31:31 +0200

    Style computer height for smaller screens

--- src/lib/compositions/ComputerSection/Computer.svelte
+++ src/lib/compositions/ComputerSection/Computer.svelte
@@ -11,7 +11,14 @@
 
 <style>
     #computer {
-        height: min(100%, 500px);
+        margin-top: var(--static-scale-300);
+        height: min(calc(100% - var(--static-scale-300)), 500px);
         width: min(100%, 500px);
+    }
+    @media (min-height: 500px) {
+        #computer {
+            height: min(100%, 500px);
+            margin-top: auto;
+        }
     }
 </style>
